7c66673bbfa145003829afdfd15bfcc89bb00d08
Kibet Brian <briankibet2040@gmail.com>
2022-03-28 23:49:20 +0300

    init cmd

--- main.go
+++ main.go
@@ -1,8 +1,35 @@
 package main
 
-import "log"
+import (
+	"log"
+	"os"
+	"os/exec"
+)
 
 
 func main (){
-	log.Println("Hey!")
+
+	if len(os.Args)<2{
+		log.Println("Check commands, try: linux-container")
+		os.Exit(1)
+	}
+	if os.Args[1]=="linux-container"{
+		StartProcess()
+	}else{
+		log.Fatalln("Check commands...Too many arguments")
+	}
+}
+
+func StartProcess (){
+
+	arguments := []string{"/bin/bash"}
+
+	cmd := exec.Command(arguments[0], arguments[1:]...)
+	cmd.Stdin, cmd.Stdout, cmd.Stderr= os.Stdin, os.Stdout, os.Stderr
+
+ 	err := cmd.Run()
+	 if err != nil {
+		 panic(err)
+	 }
+	
 }
