36f6af01136d9f4350db719376816c10e7b8202d
Kibet Brian <briankibet2040@gmail.com>
2022-03-29 22:35:15 +0300

    adds reexecute

--- main.go
+++ main.go
@@ -7,37 +7,68 @@ import (
 	"syscall"
 )
 
+const (
+	hostname = "container"
+)
+
 
 func main (){
-
+	log.Println("Arguments: ",os.Args)
 	if len(os.Args)<2{
 		log.Println("Check commands, try: linux-container")
 		os.Exit(1)
 	}
-	if os.Args[1]=="linux-container"{
-		StartProcess()
+	if os.Args[1]=="linux-container" {
+		InitProcess()
+	}
+	if os.Args[1] == "run"{
+		StartMainProcess()
 	}else{
-		log.Fatalln("Check commands...Too many arguments")
+		log.Println("Check commands and try again 2")
+		os.Exit(1)
 	}
 }
 
-func StartProcess (){
+func InitProcess (){
 	log.Println("ProcessId: ", os.Getpid())
-	arguments := []string{"/bin/bash"}
-
+	arguments := []string{"/proc/self/exe", "run"}
+	
 	cmd := exec.Command(arguments[0], arguments[1:]...)
 	cmd.Stdin, cmd.Stdout, cmd.Stderr= os.Stdin, os.Stdout, os.Stderr
-
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
-	} 
-
+	cmd.SysProcAttr = Flags(cmd)
+	
  	err := cmd.Run()
 	 if err != nil {
 		 panic(err)
 	 }
+	 log.Println("End")
+}
+
+func StartMainProcess (){
+	setHostName()
+	arguments := []string{"/bin/bash"}
+	cmd := exec.Command(arguments[0])
+	cmd.Stdin, cmd.Stdout, cmd.Stderr= os.Stdin, os.Stdout, os.Stderr
+	cmd.SysProcAttr = Flags(cmd)
+
+	err := cmd.Run()
+	if err != nil {
+		log.Fatalf("Error: %v \n",err)
+		os.Exit(1)
+	}
 	
 }
 
-func NameSpaces(){
+func Flags(cmd *exec.Cmd) *syscall.SysProcAttr{
+	attributes := &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWPID|syscall.CLONE_NEWUTS,
+	}
+	return attributes
+}
+
+func setHostName (){
+	err := syscall.Sethostname([]byte(hostname))
+	if err != nil {
+		panic(err)
+	}
 }
