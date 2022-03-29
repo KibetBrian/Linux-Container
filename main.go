584324325d16a980c7c5c9e2c34ec0c0d1d69982
Kibet Brian <briankibet2040@gmail.com>
2022-03-29 23:00:53 +0300

    refactor

--- main.go
+++ main.go
@@ -11,62 +11,69 @@ const (
 	hostname = "container"
 )
 
+func main() {
+	log.Println("Arguments: ", os.Args)
 
-func main (){
-	log.Println("Arguments: ",os.Args)
-	if len(os.Args)<2{
+	if len(os.Args) < 2 {
 		log.Println("Check commands, try: linux-container")
 		os.Exit(1)
 	}
-	if os.Args[1]=="linux-container" {
+
+	switch os.Args[1] {
+		case "linux-container":
 		InitProcess()
-	}
-	if os.Args[1] == "run"{
-		StartMainProcess()
-	}else{
-		log.Println("Check commands and try again 2")
+
+		case "run":
+
+			if os.Args[0] != "/proc/self/exe"{
+				log.Println("Invalid command, try: linux-container")
+				os.Exit(1)
+			}
+			StartMainProcess()
+
+		default:
+		log.Println("Invalid commands, try: linux-container")
 		os.Exit(1)
 	}
 }
 
-func InitProcess (){
+func InitProcess() {
 	log.Println("ProcessId: ", os.Getpid())
 	arguments := []string{"/proc/self/exe", "run"}
-	
-	cmd := exec.Command(arguments[0], arguments[1:]...)
-	cmd.Stdin, cmd.Stdout, cmd.Stderr= os.Stdin, os.Stdout, os.Stderr
-	cmd.SysProcAttr = Flags(cmd)
-	
- 	err := cmd.Run()
-	 if err != nil {
-		 panic(err)
-	 }
-	 log.Println("End")
-}
 
-func StartMainProcess (){
-	setHostName()
-	arguments := []string{"/bin/bash"}
-	cmd := exec.Command(arguments[0])
-	cmd.Stdin, cmd.Stdout, cmd.Stderr= os.Stdin, os.Stdout, os.Stderr
+	cmd := exec.Command(arguments[0], arguments[1:]...)
+	cmd.Stdin, cmd.Stdout, cmd.Stderr = os.Stdin, os.Stdout, os.Stderr
 	cmd.SysProcAttr = Flags(cmd)
 
 	err := cmd.Run()
 	if err != nil {
-		log.Fatalf("Error: %v \n",err)
-		os.Exit(1)
+		panic(err)
 	}
-	
 }
 
-func Flags(cmd *exec.Cmd) *syscall.SysProcAttr{
+func StartMainProcess() {
+	setHostName()
+	arguments := []string{"/bin/bash"}
+	cmd := exec.Command(arguments[0])
+	cmd.Stdin, cmd.Stdout, cmd.Stderr = os.Stdin, os.Stdout, os.Stderr
+	cmd.SysProcAttr = Flags(cmd)
+
+	err := cmd.Run()
+	if err != nil {
+		log.Fatalf("Error: %v \n", err)
+		os.Exit(1)
+	}
+	log.Printf("Exited the container with process id: %v \n", os.Getpid())
+}
+
+func Flags(cmd *exec.Cmd) *syscall.SysProcAttr {
 	attributes := &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWPID|syscall.CLONE_NEWUTS,
+		Cloneflags: syscall.CLONE_NEWPID | syscall.CLONE_NEWUTS,
 	}
 	return attributes
 }
 
-func setHostName (){
+func setHostName() {
 	err := syscall.Sethostname([]byte(hostname))
 	if err != nil {
 		panic(err)
