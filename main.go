a211df9089ebd1e1ae9235bcba3a8e19a55c082c
Kibet Brian <briankibet2040@gmail.com>
2022-03-29 10:58:54 +0300

    clones UTS and PIDS

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"syscall"
 )
 
 
@@ -21,15 +22,22 @@ func main (){
 }
 
 func StartProcess (){
-
+	log.Println("ProcessId: ", os.Getpid())
 	arguments := []string{"/bin/bash"}
 
 	cmd := exec.Command(arguments[0], arguments[1:]...)
 	cmd.Stdin, cmd.Stdout, cmd.Stderr= os.Stdin, os.Stdout, os.Stderr
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
+	} 
 
  	err := cmd.Run()
 	 if err != nil {
 		 panic(err)
 	 }
 	
+}
+
+func NameSpaces(){
 }
